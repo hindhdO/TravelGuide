59ce85412c00913166455f9c0e8fbaa0049c7148
hindhdO <hindhaidara14@gmail.com>
2025-02-24 22:14:02 +0000

    header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { CssBaseline, Grid } from '@mui/material';
 
-const App=() => {
-    return(
-        <div>
-            <h1> Hellooo</h1>
-        </div>
+
+import Header from './components/Header/Header';
+import List from './components/List/List';
+import Map from './components/Map/Map';
+
+const App = () => {
+    return (
+      <>
+        <CssBaseline />
+        <Header  />
+        <Grid container spacing={3} style={{ width: "100%" }}>
+          <Grid item xs={12} md={4}>
+            <List />
+          </Grid>
+        </Grid> 
+      </>
     );
-}
-
-export default App;
+  };
+  
+  export default App;
